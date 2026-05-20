3832a40f797f75384a5cd4c00a0cff159c01fc52
Shea <2788456221@qq.com>
2026-05-20 10:07:07 +0800

    Remove application-local.yml from tracking

---
+++ src/main/java/com/shea/aipassagecreator/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.shea.aipassagecreator.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域配置
+ * @author : Shea.
+ * @since : 2026/5/20 09:20
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
